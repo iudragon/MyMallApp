d7f9f944e1ed6cdf29db46ede4dfa35c191e5a1a
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-02-01 02:47:23 +0530

    SignInSystem

--- app/src/main/java/dragon/bakuman/iu/mymallapp/SignInFragment.java
+++ app/src/main/java/dragon/bakuman/iu/mymallapp/SignInFragment.java
@@ -1,11 +1,16 @@
 package dragon.bakuman.iu.mymallapp;
 
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +18,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -33,11 +45,14 @@ public class SignInFragment extends Fragment {
 
     private EditText email;
     private EditText password;
+    private ProgressBar progressBar;
 
     private ImageButton closeBtn;
     private Button signInBtn;
 
     private FirebaseAuth firebaseAuth;
+
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
 
 
     @Override
@@ -51,6 +66,7 @@ public class SignInFragment extends Fragment {
 
         email = view.findViewById(R.id.sign_in_email);
         password = view.findViewById(R.id.sign_in_password);
+        progressBar = view.findViewById(R.id.sign_in_progress_bar);
 
         closeBtn = view.findViewById(R.id.sign_in_close_btn);
         signInBtn = view.findViewById(R.id.sign_in_btn);
@@ -69,6 +85,112 @@ public class SignInFragment extends Fragment {
                 setFragment(new SignUpFragment());
             }
         });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkEmailAndPassword();
+            }
+        });
+
+    }
+
+    private void checkEmailAndPassword() {
+
+        if (email.getText().toString().matches(emailPattern)) {
+
+            if (password.length() >= 8) {
+
+                progressBar.setVisibility(View.VISIBLE);
+                signInBtn.setEnabled(false);
+                signInBtn.setTextColor(Color.argb(255, 255, 255, 255));
+
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+                            startActivity(mainIntent);
+                            getActivity().finish();
+
+                        } else {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            signInBtn.setEnabled(true);
+                            signInBtn.setTextColor(Color.rgb(255, 255, 255));
+
+                            String error = task.getException().getMessage();
+                            Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            } else {
+
+                Toast.makeText(getActivity(), "Incorrect email or password", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Incorrect email or password", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void checkInputs() {
+
+        if (!TextUtils.isEmpty(email.getText())) {
+
+            if (!TextUtils.isEmpty(password.getText())) {
+
+                signInBtn.setEnabled(true);
+                signInBtn.setTextColor(Color.rgb(255, 255, 255));
+
+            } else {
+
+                signInBtn.setEnabled(false);
+                signInBtn.setTextColor(Color.argb(255, 255, 255, 255));
+            }
+
+
+        } else {
+            signInBtn.setEnabled(false);
+            signInBtn.setTextColor(Color.argb(255, 255, 255, 255));
+
+        }
+
     }
 
     private void setFragment(Fragment fragment) {
