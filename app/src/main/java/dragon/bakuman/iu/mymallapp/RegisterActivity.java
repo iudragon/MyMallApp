862aad36a4cd4892db45f2fb779dd49da9264ebd
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-01-31 22:07:59 +0530

    Sign in and Sign up form

--- app/src/main/java/dragon/bakuman/iu/mymallapp/RegisterActivity.java
+++ app/src/main/java/dragon/bakuman/iu/mymallapp/RegisterActivity.java
@@ -3,11 +3,11 @@
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
