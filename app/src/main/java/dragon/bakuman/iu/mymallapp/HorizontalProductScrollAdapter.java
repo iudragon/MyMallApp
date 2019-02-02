f77a41b3462ba50af5e75fc217e2fe4ce8c609bf
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-02-03 00:26:11 +0530

    ProductDetailsActivity

--- app/src/main/java/dragon/bakuman/iu/mymallapp/HorizontalProductScrollAdapter.java
+++ app/src/main/java/dragon/bakuman/iu/mymallapp/HorizontalProductScrollAdapter.java
@@ -1,5 +1,6 @@
 package dragon.bakuman.iu.mymallapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -71,13 +72,22 @@ public class HorizontalProductScrollAdapter extends RecyclerView.Adapter<Horizon
         private TextView productPrice;
 
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull final View itemView) {
             super(itemView);
 
             productImage = itemView.findViewById(R.id.h_s_product_image);
             productTitle = itemView.findViewById(R.id.h_s_product_title);
             productDescription = itemView.findViewById(R.id.h_s_product_description);
             productPrice = itemView.findViewById(R.id.h_s_product_price);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent productDetailsIntent = new Intent(itemView.getContext(), ProductDetailsActivity.class);
+                    itemView.getContext().startActivity(productDetailsIntent);
+                }
+            });
 
         }
 
---
+++ app/src/main/java/dragon/bakuman/iu/mymallapp/ProductDetailsActivity.java
@@ -0,0 +1,101 @@
+package dragon.bakuman.iu.mymallapp;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+
+    private ViewPager productImagesViewPager;
+    private TabLayout viewPagerIndicator;
+    private static boolean ALREADY_ADDED_TO_WISHLIST = false;
+    private FloatingActionButton addToWishlistBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        productImagesViewPager = findViewById(R.id.product_images_viewpager);
+        viewPagerIndicator = findViewById(R.id.viewpager_indicator);
+        addToWishlistBtn = findViewById(R.id.add_to_wishlist_btn);
+
+        List<Integer> productImages = new ArrayList<>();
+        productImages.add(R.drawable.connect);
+        productImages.add(R.drawable.farmer);
+        productImages.add(R.drawable.ic_favorite);
+        productImages.add(R.drawable.ic_shopping_cart_white);
+        productImages.add(R.drawable.ic_mail_red);
+
+        ProductImagesAdapter productImagesAdapter = new ProductImagesAdapter(productImages);
+        productImagesViewPager.setAdapter(productImagesAdapter);
+
+        viewPagerIndicator.setupWithViewPager(productImagesViewPager, true);
+        addToWishlistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ALREADY_ADDED_TO_WISHLIST) {
+
+                    ALREADY_ADDED_TO_WISHLIST = false;
+
+                    addToWishlistBtn.setSupportImageTintList(ColorStateList.valueOf(Color.parseColor("#C9ABAB")));
+
+                } else {
+
+                    ALREADY_ADDED_TO_WISHLIST = true;
+
+                    addToWishlistBtn.setSupportImageTintList(getResources().getColorStateList(R.color.colorAccent));
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_and_cart_icon, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+
+            finish();
+            return true;
+        } else if (id == R.id.main_search_icon) {
+            return true;
+        } else if (id == R.id.main_cart_icon) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
