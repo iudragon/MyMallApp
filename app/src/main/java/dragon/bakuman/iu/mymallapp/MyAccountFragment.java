66cfff6e0acb0bc6da85f05189fbb3cd64fc9d80
iudragon <iudragon32@gmail.com>
2019-04-03 16:09:29 +0530

    12ContributionAbout

--- app/src/main/java/dragon/bakuman/iu/mymallapp/MyAccountFragment.java
+++ app/src/main/java/dragon/bakuman/iu/mymallapp/MyAccountFragment.java
@@ -2,7 +2,6 @@ package dragon.bakuman.iu.mymallapp;
 
 
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,10 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import static dragon.bakuman.iu.mymallapp.DeliveryActivity.SELECT_ADDRESS;
-import static dragon.bakuman.iu.mymallapp.RegisterActivity.setSignUpFragment;
 
 
 /**
