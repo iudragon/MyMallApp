83f0a9221651cc1780939f354803efd79a60cfa0
iudragon <iudragon32@gmail.com>
2019-04-03 15:12:48 +0530

    12COntributionsAbout

--- app/src/main/java/dragon/bakuman/iu/mymallapp/MyAccountFragment.java
+++ app/src/main/java/dragon/bakuman/iu/mymallapp/MyAccountFragment.java
@@ -1,6 +1,7 @@
 package dragon.bakuman.iu.mymallapp;
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,8 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import static dragon.bakuman.iu.mymallapp.DeliveryActivity.SELECT_ADDRESS;
+import static dragon.bakuman.iu.mymallapp.RegisterActivity.setSignUpFragment;
 
 
 /**
@@ -24,6 +28,9 @@ public class MyAccountFragment extends Fragment {
 
     private Button viewAllAddressButton;
 
+    private Dialog contributionDialog;
+    private ImageView contribFlaticonCircle;
+
     public static final int MANAGE_ADDRESS = 1;
 
 
@@ -31,7 +38,40 @@ public class MyAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_my_account, container, false);
+        final View view =  inflater.inflate(R.layout.fragment_my_account, container, false);
+
+        contribFlaticonCircle = view.findViewById(R.id.contribFlaticonCircle);
+        contribFlaticonCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contributionDialog = new Dialog(view.getContext());
+                contributionDialog.setContentView(R.layout.contribution_dialog);
+                contributionDialog.setCancelable(true);
+
+                contributionDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                contributionDialog.show();
+
+
+
+//                Button contributionBtn = contributionDialog.findViewById(R.id.contrib);
+//
+//                contributionBtn.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Toast.makeText(view.getContext(), "Cliked the contrib btn", Toast.LENGTH_SHORT).show();
+//
+//
+//                        contributionDialog.dismiss();
+//                    }
+//                });
+
+            }
+        });
+
+
+
+
 
 //        viewAllAddressButton = view.findViewById(R.id.view_all_addresses_btn);
 //        viewAllAddressButton.setOnClickListener(new View.OnClickListener() {
